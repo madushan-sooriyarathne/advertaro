b778ab8b96bad96d61845138c645500cfc036dc2
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-11-04 19:34:35 +0530

    Changed some colors and one font family

--- styles/theme.js
+++ styles/theme.js
@@ -1,18 +1,20 @@
 const theme = {
-  colorPrimary: "#f77f00",
-  colorPrimaryDark: "#d62828",
-  colorPrimaryLite: "#fcbf49",
-  colorSecondary: "#27374a",
+  colorPrimary: "#e85d04",
+  colorPrimaryDark: "#370617",
+  colorPrimaryLite: "#ffba08",
+  colorSecondary: "#3d405b",
   colorTertiary: "#eae2b7",
   colorWhite: "#fff",
   colorBlack: "#000",
+  colorGray: "#b7b7a4",
+  colorBg: "#f8edeb",
   fontPrimary: "'Montserrat', sans-serif",
-  fontSecondary: "'Philosopher', sans-serif",
+  fontSecondary: "'Overpass', sans-serif;",
   boxShadowNormal: "0 4px 8px 4px rgba(0, 0, 0, 0.1)",
   boxShadowHover: "0 8px 16px 4px rgba(0, 0, 0, 0.1)",
   gridTemplateColumns:
     "[full-start] minmax(5rem, 1fr) [content-start] 170rem [content-end] minmax(5rem, 1fr) [full-end] ",
-  navBarHeight: "11rem",
+  navBarHeight: "10rem",
 };
 
 export default theme;
