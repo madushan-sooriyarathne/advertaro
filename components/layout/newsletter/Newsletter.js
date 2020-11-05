8b2b8a3e7abc86eb7ceea7b4136b9da35e63871b
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-11-05 21:01:02 +0530

    added form submit handler for the newsletter component

--- components/layout/newsletter/Newsletter.js
+++ components/layout/newsletter/Newsletter.js
@@ -1,19 +1,48 @@
-import { NewsletterSection, FormGroup, TitleGroup } from "./NewsletterStyles";
+import { useContext } from "react";
+import { SnackBarDispatchContext } from "../../../context/SnackBarContext";
+import useInputState from "../../../hooks/UseInputState";
 
 import MainHeading from "../../headings/main-heading/MainHeading";
 import SecondaryHeading from "../../headings/secondary-heading/SecondaryHeading";
 import InputField from "../../input-field/InputField";
 import SubmitButton from "../../buttons/submit-button/SubmitButton";
 
+import { NewsletterSection, FormGroup, TitleGroup } from "./NewsletterStyles";
 const Newsletter = () => {
+  // context
+  const setMessage = useContext(SnackBarDispatchContext);
+
+  // state
+  const [email, updateEmail, resetEmail] = useInputState("");
+
+  const handleSubmit = (event) => {
+    // TODO: handle saving the submitted email to mailchimp or our own DB
+    // until that process run show  a loading spinner in the button
+
+    event.preventDefault();
+    resetEmail();
+    setMessage(
+      "Thanks for subscribing to our newsletter. A confirmation email has been sent!"
+    );
+    setTimeout(() => {
+      setMessage();
+    }, 5000);
+  };
+
   return (
     <NewsletterSection>
       <TitleGroup>
         <MainHeading>Receive Continuous updates</MainHeading>
         <SecondaryHeading>Subscribe to our Newsletter</SecondaryHeading>
       </TitleGroup>
-      <FormGroup>
-        <InputField type="text" name="Your Email" id="newsletter"></InputField>
+      <FormGroup onSubmit={handleSubmit}>
+        <InputField
+          type="email"
+          name="Your Email"
+          id="newsletter"
+          value={email}
+          handleUpdate={updateEmail}
+        ></InputField>
         <SubmitButton>Subscribe</SubmitButton>
       </FormGroup>
     </NewsletterSection>
