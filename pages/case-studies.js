59057b77aca02b27d4a3e2c7b7495bbff594c964
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-11-08 00:03:46 +0530

    CaseStudiesSection is added to case-studies page. also the Header component was removed from case-studies page

---
+++ pages/case-studies.js
@@ -0,0 +1,31 @@
+import { fetchCaseStudies } from "../contentful/caseStudies";
+
+import Page from "../components/layout/page/Page";
+import CaseStudiesSection from "../components/layout/case-studies-section/CaseStudiesSection";
+
+const CaseStudies = ({ caseStudies }) => {
+  console.log(caseStudies);
+  return (
+    <Page>
+      <CaseStudiesSection caseStudies={caseStudies} />
+    </Page>
+  );
+};
+
+const getStaticProps = async () => {
+  const res = await fetchCaseStudies();
+  const caseStudies = await res.map((entry) => ({
+    id: entry.sys.id,
+    ...entry.fields,
+  }));
+
+  return {
+    props: {
+      caseStudies,
+    },
+  };
+};
+
+export { getStaticProps };
+
+export default CaseStudies;
