f8a1487b1096b13802229b29e656826bb9ac42c9
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2020-11-18 00:56:29 +0530

    layout style chaned in HeroBanner Component

--- components/layout/HeroBanner/HeroBanner.js
+++ components/layout/HeroBanner/HeroBanner.js
@@ -1,7 +1,23 @@
+import Parallax from "parallax-js";
+import { useEffect } from "react";
+
 import CTAButton from "../../buttons/ctaButton/CTAButton";
-import { Container, CTAGroup, Heading, SubHeading } from "./HeroBannerStyles";
+
+import {
+  Container,
+  CTAGroup,
+  Heading,
+  ImageWrapper,
+  Image,
+  SubHeading,
+} from "./HeroBannerStyles";
 
 const HeroBanner = () => {
+  useEffect(() => {
+    let scene = document.getElementById("scene");
+    let parallaxInstance = new Parallax(scene);
+  });
+
   return (
     <Container>
       <CTAGroup>
@@ -9,6 +25,13 @@ const HeroBanner = () => {
         <Heading>Digital</Heading>
         <CTAButton text="Get in Touch" url="/contact"></CTAButton>
       </CTAGroup>
+      <ImageWrapper id="scene">
+        <Image
+          src="/static/img/instagram-phone.png"
+          alt="display phone"
+          data-depth="0.3"
+        />
+      </ImageWrapper>
     </Container>
   );
 };
--- components/layout/HeroBanner/HeroBannerStyles.js
+++ components/layout/HeroBanner/HeroBannerStyles.js
@@ -3,54 +3,109 @@ import styled from "styled-components";
 const Container = styled.section`
   grid-column: full-start / full-end;
 
-  display: flex;
+  display: grid;
+
+  grid-template-columns: repeat(2, minmax(max-content, 1fr));
+  grid-auto-rows: max-content;
+  grid-template-areas: "ct im";
 
   justify-content: center;
   align-items: center;
 
   height: 100vh;
-  background: linear-gradient(
+  background: url("/static/img/svg/hero-bg.svg"),
+    linear-gradient(
       90deg,
-      rgba(0, 0, 0, 0.7) 0%,
-      rgba(37, 16, 4, 0.7) 35%,
-      rgba(0, 0, 0, 0.7) 100%
-    ),
-    url("/static/img/hero-background.jpg");
+      rgba(0, 0, 0, 1) 0%,
+      rgba(37, 16, 4, 1) 35%,
+      rgba(0, 0, 0, 1) 100%
+    );
   background-repeat: no-repeat;
   background-size: cover;
-  background-position: center;
+  background-position: bottom;
 
   padding: 10rem 5rem;
+
+  & > * {
+    padding: 10rem;
+  }
+
+  ${(props) => props.theme.responsiveMediumHigh} {
+    height: 80vh;
+  }
+
+  ${(props) => props.theme.responsiveMedium} {
+    height: 70vh;
+    & > * {
+      padding: 5rem;
+    }
+  }
+
+  ${(props) => props.theme.responsiveLower} {
+    height: auto;
+    grid-template-columns: 1fr;
+    grid-template-areas:
+      "ct"
+      "im";
+    padding: 5rem;
+  }
 `;
 
 const CTAGroup = styled.div`
-  grid-column: center-start / center-end;
+  grid-area: ct;
 
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: start;
   justify-content: center;
+  padding: 10rem 10rem 10rem 20rem;
 
   & > * {
     text-align: center;
   }
+
+  ${(props) => props.theme.responsiveLower} {
+    min-height: 50vh;
+    align-items: center;
+    padding: 10rem;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  grid-area: im;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Image = styled.img`
+  height: 65rem;
+
+  ${(props) => props.theme.responsiveHighest} {
+    height: 60rem;
+    align-self: flex-end;
+  }
+
+  ${(props) => props.theme.responsiveLower} {
+    min-height: 65rem;
+    align-items: center;
+  }
 `;
 
 const Heading = styled.h2`
-  font-size: 7rem;
-  font-weight: 900;
+  font-size: 10rem;
+  font-weight: 700;
   font-family: ${(props) => props.theme.fontSecondary};
-  letter-spacing: 3px;
-  text-transform: uppercase;
+  letter-spacing: 0.5px;
   color: ${(props) => props.theme.colorPrimary};
   margin-bottom: 2rem;
 `;
 
 const SubHeading = styled.h1`
   font-size: 3rem;
-  font-weight: 400;
+  font-weight: 300;
   font-family: ${(props) => props.theme.fontPrimary};
-  letter-spacing: 2px;
+  letter-spacing: 0.5px;
   color: ${(props) => props.theme.colorWhite};
 
   @media only screen and (max-width: 26.875em) {
@@ -58,4 +113,4 @@ const SubHeading = styled.h1`
   }
 `;
 
-export { Container, CTAGroup, Heading, SubHeading };
+export { Container, CTAGroup, ImageWrapper, Image, Heading, SubHeading };
