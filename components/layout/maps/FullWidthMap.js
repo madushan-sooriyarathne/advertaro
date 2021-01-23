88cd7bf30560f043b42ec9fb57ff5dde8228932b
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-01-23 22:12:14 +0530

    implemented mapbox map

--- components/layout/maps/FullWidthMap.js
+++ components/layout/maps/FullWidthMap.js
@@ -1,21 +1,28 @@
-import {
-  withScriptjs,
-  GoogleMap,
-  Marker,
-  withGoogleMap,
-} from "react-google-maps";
+import mapboxgl from "mapbox-gl";
+import { MapContainer } from "./FullWidthMapStyles";
+import { useEffect } from "react";
 
-const FullWidthMap = withScriptjs(
-  withGoogleMap((props) => {
-    return (
-      <GoogleMap
-        defaultZoom={15}
-        defaultCenter={{ lat: 6.840108, lng: 79.871117 }}
-      >
-        <Marker position={{ lat: 6.840108, lng: 79.871117 }} />
-      </GoogleMap>
-    );
-  })
-);
+const FullWidthMap = () => {
+  useEffect(() => {
+    // Initialize mapbox map
+    // configure mapbox with api key
+    mapboxgl.accessToken = process.env.MAPBOX_API_KEY;
+
+    // draw mapbox on page
+    const map = new mapboxgl.Map({
+      container: "map-container",
+      style: "mapbox://styles/mapbox/light-v10",
+      center: [79.871117, 6.840108],
+      zoom: 12,
+      pitch: 50,
+      minZoom: 10,
+    });
+
+    // Add marker
+    new mapboxgl.Marker().setLngLat([79.871117, 6.840108]).addTo(map);
+  });
+
+  return <MapContainer id="map-container"></MapContainer>;
+};
 
 export default FullWidthMap;
--- components/layout/maps/FullWidthMapStyles.js
+++ components/layout/maps/FullWidthMapStyles.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 const MapContainer = styled.div`
   width: 100%;
-  height: 60rem;
+  height: 100%;
+  min-height: 60rem;
 `;
 
 export { MapContainer };
